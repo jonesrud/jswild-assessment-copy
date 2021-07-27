588fe689fb7f62e63f2f013dbc2928c459d01eae
Rudi Jones <r20.marie@gmail.com>
2021-07-27 16:27:45 -0400

    JS in the wild PT1

--- code.js
+++ code.js
@@ -2,3 +2,17 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+
+function onGeolocationSuccess(data) {
+    console.log(data)
+}
+
+function onGeolocationError(){
+    const fallbackLocation = {latitude: 42.1896358, longitude: -83.3119488 }
+    console.log("Cannot find geoposition")
+}
+
+navigator.geolocation.getCurrentPosition(
+    function(coordinates){console.log (coordinates)},
+
+)
